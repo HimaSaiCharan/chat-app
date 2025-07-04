0f7f199f41ab7b84b9101dfe4c09a0d7d5c8fd83
Bhagya <146413665+snehakarada@users.noreply.github.com>
2025-07-04 11:41:23 +0530

    configuration changed

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -2,8 +2,7 @@ import { Test, TestingModule } from "@nestjs/testing";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
 import { DatabaseService } from "src/database/database.service";
-import { Collection } from "mongodb";
-import { mock } from "node:test";
+import { UserInfo } from "../types";
 
 describe("AuthController", () => {
   let authController: AuthController;
@@ -80,6 +79,22 @@ describe("AuthController", () => {
         message: "malli is already used",
         url: null,
       });
+    });
+  });
+
+  describe("signinUser", () => {
+    it("should return success response when user is already there", async () => {
+      const mockFind = jest.fn();
+      const mockCollection = {
+        find: mockFind,
+      };
+      const res = {
+        json: jest.fn(),
+      };
+
+      jest.spyOn(authservice as any, "getDb").mockReturnValue(mockCollection);
+      await authservice.signinUser("malli", "123", res);
+      expect(res.json).toHaveBeenCalledWith({ isExist: false });
     });
   });
 });
